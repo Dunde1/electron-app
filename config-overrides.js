e3c2d8e870156040914af0c4692affd57c4d62f6
dunde <chldlsrb1000@naver.com>
2023-04-30 20:22:08 +0900

    update by eslint, prettier

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = (config, _env) => {
   config.resolve.alias = {
-    '@src': path.resolve(__dirname, 'src/')
-  }
+    '@src': path.resolve(__dirname, 'src/'),
+  };
 
   return config;
 };
